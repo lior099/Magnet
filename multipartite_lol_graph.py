e0912d113196626866a86c095b3a1217d1881919
Haim Isakov <58917696+HaimIsakov@users.noreply.github.com>
2020-12-02 16:36:29 +0200

    Update multipartite_lol_graph.py

--- multipartite_lol_graph.py
+++ multipartite_lol_graph.py
@@ -1,24 +1,24 @@
 import os
 import numpy as np
 from collections import OrderedDict
-from lol_graph import lol_graph
+from lol_graph_directed import *
 import csv
 
 
-class Multipartite_Lol(lol_graph):
-    def __init__(self, groups_number=0, directed=False, weighted=False):
-        super().__init__(directed=directed, weighted=weighted)
+class MultipartiteLol(DLGW):
+    def __init__(self, groups_number=0, weighted=True):
+        super().__init__(weighted=weighted)
         self.groups_number = groups_number
         self.nodes_type_dict = {}
 
-    def convert_with_csv(self, files_name, graphs_directions=None, directed=False, weighted=False, header=True):
+    def convert_with_csv(self, files_name, graphs_directions=None, header=True):
         s = set()
         for t in graphs_directions:
             s.add(t[0])
             s.add(t[1])
         self.groups_number = len(s)
 
-        self._map_node_to_number = OrderedDict()
+        # self._map_node_to_number = OrderedDict()
         graph = []
         for i in range(len(files_name)):
             file = files_name[i]
@@ -29,26 +29,21 @@ class Multipartite_Lol(lol_graph):
                 for edge in datareader:
                     named_edge = [str(graphs_directions[i][0]) + "_" + edge[0],
                                   str(graphs_directions[i][1]) + "_" + edge[1]]
-                    if weighted:
+                    if self.is_weighted():
                         named_edge.append(float(edge[2]))
                     graph.append(named_edge)
                 csvfile.close()
-        self.convert(graph, directed, weighted)
+        self.convert(graph)
 
     def return_node_type(self, node):
         return self.nodes_type_dict[node]['type']
 
     def copy(self):
-        new_mp_lol_graph = Multipartite_Lol()
-        new_mp_lol_graph._index_list = self._index_list.copy()
-        new_mp_lol_graph._neighbors_list = self._neighbors_list.copy()
-        new_mp_lol_graph._weights_list = self._weights_list.copy()
-        new_mp_lol_graph._map_node_to_number = self._map_node_to_number.copy()
-        new_mp_lol_graph._map_number_to_node = self._map_number_to_node.copy()
-        new_mp_lol_graph.directed = self.directed
-        new_mp_lol_graph.weighted = self.weighted
+        new_mp_lol_graph = MultipartiteLol()
+        new_mp_lol_graph.reversed_lol = self.reversed_lol.copy()
+        new_mp_lol_graph.lol_directed = self.lol_directed.copy()
         new_mp_lol_graph.groups_number = self.groups_number
-        new_mp_lol_graph.nodes_type_dict = self.nodes_type_dict
+        new_mp_lol_graph.nodes_type_dict = self.nodes_type_dict.copy()
         return new_mp_lol_graph
 
     def set_nodes_type_dict(self):
@@ -63,10 +58,10 @@ class Multipartite_Lol(lol_graph):
 
 
 if __name__ == '__main__':
-    list_of_list_graph = Multipartite_Lol()
+    list_of_list_graph = MultipartiteLol()
     rootDir = os.path.join("PathwayProbabilitiesCalculation", "data", "toy_network_1")
     graph_filenames = [os.path.join(dirpath, file) for (dirpath, dirnames, filenames) in
                        os.walk(rootDir) for file in filenames]
     graph_filenames.sort()
-    list_of_list_graph.convert_with_csv(graph_filenames, [(0, 1), (1, 0), (1, 2), (2, 1), (2, 0), (0, 2)], True, True)
-    print(list_of_list_graph.nodes())
+    list_of_list_graph.convert_with_csv(graph_filenames, [(0, 1), (1, 0), (1, 2), (2, 1), (2, 0), (0, 2)])
+    print(list_of_list_graph.predecessors("1_3"))
